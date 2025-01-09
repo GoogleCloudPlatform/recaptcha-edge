e0e284076388002be23fded4427a97ff222e5027
Brandon Surmanski <b.surmanski@gmail.com>
2025-01-09 10:33:20 -0500

    try/catch opening https cert files

--- integration/mock_backend/index.js
+++ integration/mock_backend/index.js
@@ -5,12 +5,15 @@ const config = require("./config");
 const https = require("https");
 const fs = require("fs");
 const app = express();
-var key = fs.readFileSync(__dirname + "/certs/selfsigned.key");
-var cert = fs.readFileSync(__dirname + "/certs/selfsigned.crt");
-var options = {
-  key: key,
-  cert: cert,
-};
+var https_options = null;
+try {
+  var key = fs.readFileSync(__dirname + "/certs/selfsigned.key");
+  var cert = fs.readFileSync(__dirname + "/certs/selfsigned.crt");
+  https_options = {
+    key: key,
+    cert: cert,
+  };
+}
 
 app.engine("handlebars", handlebars.engine({ defaultLayout: false })); // No default layout here
 app.set("view engine", "handlebars");
@@ -40,7 +43,7 @@ app.listen(config.http_port, () => {
   console.log(`mock backend listening on port ${config.http_port}`);
 });
 
-if (config.https_port > 0) {
+if (config.https_port > 0 && https_options) {
   var server = https.createServer(options, app);
   server.listen(config.https_port, () => {
     console.log(`mock backend https starting on port " ${config.https_port}`);
