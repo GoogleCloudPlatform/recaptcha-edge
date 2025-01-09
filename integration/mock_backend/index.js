ad37e61f78f2885bbbb813d46807fa0837e75903
Brandon Surmanski <b.surmanski@gmail.com>
2025-01-09 10:07:02 -0500

    add https support to mock_backend

--- integration/mock_backend/index.js
+++ integration/mock_backend/index.js
@@ -1,33 +1,50 @@
-const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require("path");
-const config = require("./config");
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const config = require('./config');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
 
-app.engine("handlebars", handlebars.engine({ defaultLayout: false })); // No default layout here
-app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public")); // Serve static files from public folder
+app.engine('handlebars', handlebars.engine({defaultLayout: false})); // No default layout here
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public')); // Serve static files from public folder
 
-app.get("/token/action", (req, res) => {
-  res.render("action", { siteKey: config.actionSiteKey });
+app.get('/token/action', (req, res) => {
+  res.render('action',   
+    { siteKey: config.actionSiteKey });
 });
 
-app.get("/token/session", (req, res) => {
-  res.render("session", { siteKey: config.sessionSiteKey });
+app.get('/token/session', (req, res) => {
+  res.render('session',   
+    { siteKey: config.sessionSiteKey });
 });
 
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   res.send({
     url: req.url,
     method: req.method,
     body: req.body,
     headers: req.headers,
     query: req.query,
-    params: req.params,
+    params: req.params
   });
 });
 
-app.listen(config.port, () => {
-  console.log(`mock backend listening on port ${config.port}`);
+app.listen(config.http_port, () => {
+  console.log(`mock backend listening on port ${config.http_port}`);
 });
+
+if (config.https_port > 0) {
+  var server = https.createServer(options, app);
+  server.listen(config.https_port, () => {
+    console.log(`mock backend https starting on port " ${config.https_port}`)
+  });
+}
