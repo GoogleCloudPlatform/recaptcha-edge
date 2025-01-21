701837855edf07cd0fac722d1bf98d5ef43fd9a8
yaya1721 <Sonyahyun.hs@gmail.com>
2025-01-21 15:32:57 -0500

    update waf configs

--- bindings/cloudflare/src/index.ts
+++ bindings/cloudflare/src/index.ts
@@ -114,7 +114,7 @@ export function recaptchaConfigFromEnv(env: Env): RecaptchaConfig {
     expressSiteKey: env.EXPRESS_SITE_KEY,
     sessionSiteKey: env.SESSION_SITE_KEY,
     challengePageSiteKey: env.CHALLENGE_PAGE_SITE_KEY,
-    enterpriseSiteKey: env.V3_SITE_KEY,
+    enterpriseSiteKey: env.ENTERPRISE_SITE_KEY,
     recaptchaEndpoint: env.RECAPTCHA_ENDPOINT ?? DEFAULT_RECAPTCHA_ENDPOINT,
     sessionJsInjectPath: env.SESSION_JS_INSTALL_PATH,
     debug: env.DEBUG ?? false,
