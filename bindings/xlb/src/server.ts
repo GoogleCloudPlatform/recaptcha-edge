5c9be357f22ef748e5d5435891946df6f3f7efc0
Jordan Stinson <jstinson@google.com>
2025-04-04 13:17:57 -0400

    Renaming resources to 'edge'. Cleaning up input defaults

--- bindings/xlb/src/server.ts
+++ bindings/xlb/src/server.ts
@@ -3,17 +3,17 @@ import * as server from "./index";
 
 function getConfig(): RecaptchaConfig {
     return {
-        projectNumber: parseInt(process.env.PROJECT_NUMBER || ""),
+        projectNumber: parseInt(process.env.PROJECT_NUMBER || "") || 0,
         apiKey: process.env.API_KEY || "",
-        actionSiteKey: process.env.ACTION_SITE_KEY || "",
+        actionSiteKey: process.env.ACTION_SITE_KEY || undefined,
         expressSiteKey: process.env.EXPRESS_SITE_KEY || undefined,
         sessionSiteKey: process.env.SESSION_SITE_KEY || undefined,
-        challengePageSiteKey: process.env.CHALLENGE_PAGE_SITE_KEY || "",
+        challengePageSiteKey: process.env.CHALLENGE_PAGE_SITE_KEY || undefined,
         enterpriseSiteKey: process.env.ENTERPRISE_SITE_KEY || undefined,
         recaptchaEndpoint: process.env.RECAPTCHA_ENDPOINT || "",
-        sessionJsInjectPath: process.env.SESSION_JS_INJECT_PATH || undefined,
-        debug: true,
-        unsafe_debug_dump_logs: true,
+        sessionJsInjectPath: process.env.SESSION_JS_INSTALL_PATH || undefined,
+        debug: (process.env.DEBUG ?? "false") == "true",
+        unsafe_debug_dump_logs: false,
         strict_cookie: false
       };
 }
--- bindings/xlb/terraform/inputs.tf
+++ bindings/xlb/terraform/inputs.tf
@@ -18,7 +18,7 @@ variable "extension_cel_match" {
     type = string
 }
 
-variable "waf_container" {
+variable "edge_container" {
     type = string
 }
 
@@ -29,14 +29,15 @@ variable "source_root" {
 
 variable "callout_config" {
     type = object({
-        project_number          = number
-        api_key                 = string
-        action_site_key         = optional(string, "")
-        express_site_key        = optional(string, "")
-        challenge_page_site_key = optional(string, "")
-        enterprise_site_key     = optional(string, "")
-        recaptcha_endpoint      = optional(string, "https://public-preview-recaptchaenterprise.googleapis.com")
-        session_js_inject_path  = optional(string, "")
-        session_site_sey        = optional(string, "")
+        project_number           = number
+        api_key                  = string
+        action_site_key          = optional(string, null)
+        express_site_key         = optional(string, null)
+        challenge_page_site_key  = optional(string, null)
+        enterprise_site_key      = optional(string, null)
+        recaptcha_endpoint       = optional(string, "https://public-preview-recaptchaenterprise.googleapis.com")
+        session_js_install_path  = optional(string, null)
+        session_site_sey         = optional(string, null)
+        debug                    = optional(bool, false)
     })
 }
